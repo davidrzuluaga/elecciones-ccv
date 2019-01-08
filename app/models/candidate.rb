1da62aec09ca3073894fef0c06ef9bda84957528
davidrzuluaga <git@davidrzuluaga.com>
2019-01-07 19:27:31 -0500

    candidate

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,5 @@
 class Candidate < ApplicationRecord
+  belongs_to :position
   belongs_to :group
   belongs_to :election
 end
--- db/migrate/20190108002622_create_candidates.rb
+++ db/migrate/20190108002622_create_candidates.rb
@@ -2,7 +2,7 @@ class CreateCandidates < ActiveRecord::Migration[5.2]
   def change
     create_table :candidates do |t|
       t.string :name
-      t.string :position
+      t.references :position, foreign_key: true
       t.references :group, foreign_key: true
       t.references :election, foreign_key: true
 
